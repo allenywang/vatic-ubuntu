00084aafe13463a19ef64590657e0937c6bcf8ab
Allen Wang <allen.y.w@berkeley.edu>
2016-05-20 20:40:41 -0700

    Added setup

---
+++ setup.sh
@@ -0,0 +1,20 @@
+sudo apt-get update
+sudo apt-get install python-setuptools
+sudo apt-get install git
+sudo apt-get install build-essential
+sudo apt-get install autoconf libtool pkg-config python-opengl python-imaging pythonpyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev
+sudo apt-get install libapache2-mod-wsgi
+sudo apt-get install python-dev libxml2-dev libxslt-dev
+sudo apt-get install python-mysqldb
+sudo easy_install munkres
+sudo apt-get install python-pip
+sudo pip2 install –upgrade parsedatetime
+sudo ln -s /usr/lib/x86 64-linux-gnu/libjpeg.so /usr/lib
+sudo ln -s /usr/lib/x86 64-linux-gnu/libfreetype.so /usr/lib
+sudo ln -s /usr/lib/x86 64-linux-gnu/libz.so /usr/lib
+sudo apt-get remove --purge ffmpeg
+sudo apt-add-repository ppa:mc3man/trusty-media
+sudo apt-get install ffmpeg
+sudo pip install cython==0.20
+./vatic-install.sh
+
