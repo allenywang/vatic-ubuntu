dddc10f29631ec452e4e740d48a131e6cd5a3991
Allen Wang <allen.y.w@berkeley.edu>
2016-05-20 21:56:31 -0700

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -4,10 +4,13 @@ sudo apt-get install git
 sudo apt-get install build-essential
 sudo apt-get install autoconf libtool pkg-config python-opengl python-imaging pythonpyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev
 sudo apt-get install libapache2-mod-wsgi
+sudo apt-get install mysql-client-core-5.5
 sudo apt-get install python-dev libxml2-dev libxslt-dev
 sudo apt-get install python-mysqldb
 sudo easy_install munkres
 sudo apt-get install python-pip
+sudo apt-get install mysql-server
+sudo pip install parsedatetime
 sudo pip2 install –upgrade parsedatetime
 sudo ln -s /usr/lib/x86 64-linux-gnu/libjpeg.so /usr/lib
 sudo ln -s /usr/lib/x86 64-linux-gnu/libfreetype.so /usr/lib
